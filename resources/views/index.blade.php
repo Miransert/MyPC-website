8dd2c41c3c5ab28b765c4b3820c4a9c2b508f577
Niels Faurskov <niels@faurskov.dev>
2022-09-22 20:02:42 +0200

    Initial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('styles/primary.css') }}" />
+    <link rel="stylesheet" href="{{ asset('styles/custom.css') }}" />
+</head>
+
+<body>
+    <nav>
+        <div class="container">
+            <a id="logo">
+                <h2>MyPC</h2>
+                <h3>WebStore</h3>
+            </a>
+            <div class="cart">
+                <span>In cart: </span><span id="cart-count"> 0</span>
+            </div>
+        </div>
+    </nav>
+    <section class="container">
+        <!--Your HTML-->
+    </section>
+</body>
+
+</html>
